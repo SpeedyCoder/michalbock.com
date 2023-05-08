a46115903c86001b8973e175c302ac431b16efe4
SpeedyCoder <michal.bock@deliveroo.co.uk>
2023-05-08 20:11:57 +0100

    fix: use menu component

--- ui/src/components/Navigation/index.tsx
+++ ui/src/components/Navigation/index.tsx
@@ -1,22 +1,46 @@
 import * as React from 'react'
-import {Box, Center, Flex, Heading, Link, Stack} from '@chakra-ui/react'
+import {
+    Box,
+    Flex,
+    Heading,
+    IconButton,
+    Link,
+    Menu,
+    MenuButton,
+    MenuDivider,
+    MenuItem,
+    MenuList,
+    Stack
+} from '@chakra-ui/react'
 import {ColorModeSwitcher} from './ColorModeSwitcher'
 import {Link as RouteLink} from 'react-router-dom'
+import {FaBars} from "react-icons/fa"
 
-export const Navigation :React.FC = () => (
+export const Navigation: React.FC = () => (
     <Box textAlign="center">
         <Flex h={16} alignItems='center' justifyContent='space-around'>
             <Link as={RouteLink} to='/'><Heading size='lg' fontSize='20px'>MICHALBOCK.COM</Heading></Link>
 
             <Flex alignItems={'center'}>
                 <Stack direction={'row'} spacing={7}>
-                    <Center>
-                        <Link as={RouteLink} to='/posts'><b>Blog</b></Link>
-                    </Center>
-                    <Center>
-                        <Link as={RouteLink} to='/about'><b>About</b></Link>
-                    </Center>
-                    <ColorModeSwitcher></ColorModeSwitcher>
+                    <Menu placement='bottom-end'>
+                        <MenuButton
+                            as={IconButton}
+                            aria-label='Options'
+                            icon={<FaBars/>}
+                            variant='outline'
+                        />
+                        <MenuList>
+                            <MenuItem as={RouteLink} to='/posts'>
+                                Blog
+                            </MenuItem>
+                            <MenuItem as={RouteLink} to='/about'>
+                                About
+                            </MenuItem>
+                            <MenuDivider/>
+                            <ColorModeSwitcher/>
+                        </MenuList>
+                    </Menu>
                 </Stack>
             </Flex>
         </Flex>
